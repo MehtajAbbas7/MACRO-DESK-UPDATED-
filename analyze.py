430fe616976c7dc3c22b41b4f0850b99269e7333
MehtajAbbas7 <mehtajabbas12@gmail.com>
2026-06-22 23:39:12 +0100

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -36,7 +36,8 @@ req = urllib.request.Request(
     data=body,
     headers={
         "Content-Type": "application/json",
-        "Authorization": "Bearer " + key
+        "Authorization": "Bearer " + key,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
 )
 
